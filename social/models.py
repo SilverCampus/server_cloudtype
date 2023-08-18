214573b675072ce913356146f74cfe622416f03d
JTH <nex_david@naver.com>
2023-08-18 18:52:16 +0900

    sdfasdfasdf

--- social/models.py
+++ social/models.py
@@ -7,15 +7,14 @@ class Hashtag(models.Model):
     def __str__(self):
         return self.name
 
+
 class BoardPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # title = models.CharField(max_length=200, null=True, blank=True)
     content = models.TextField()
-    # image = models.ImageField(upload_to='social_posts/images/', null=True, blank=True)  
+
     video = models.FileField(upload_to='social_posts/videos/', null=True, blank=True)
     video_thumbnail = models.ImageField(upload_to='social_posts/thumbnails/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    # hashtags = models.ManyToManyField(Hashtag, related_name='posts')
     hashtags = models.ForeignKey(Hashtag, related_name='posts', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
